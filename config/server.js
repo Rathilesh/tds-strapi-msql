e3b503e7ba287eda3da55f65929eb7f77576a98e
Rathilesh <rathilesh.c@gmail.com>
2021-04-15 22:35:01 +0530

    initial commit with mysql db

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fda465e6df7bb275adfa9f73b636ed2a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '30e02089-3b27-4f45-96a6-45868b14af7d'
+};
