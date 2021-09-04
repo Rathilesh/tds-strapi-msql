b141b886725938cb79bf8477b362af39fe539f05
Rathilesh <rathilesh.c@gmail.com>
2021-09-04 23:14:46 +0530

    after changing mysql2 plugin

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'tds-admin-strapi'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        client: 'mysql2',
+        host: env('DATABASE_HOST', 'db-mysql-tds-do-user-9345893-0.b.db.ondigitalocean.com'),
+        port: env.int('DATABASE_PORT', 25060),
+        database: env('DATABASE_NAME', 'tds-live'),
+        username: env('DATABASE_USERNAME', 'doadmin'),
+        password: env('DATABASE_PASSWORD', 'w4c0yvi6ozyc990h'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
